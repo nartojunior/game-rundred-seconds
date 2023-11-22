d2d96eb8c1f849a07e1eb26de80cb627615cb250
Narto Junior <nartojunior@outlook.com>
2023-11-22 18:37:57 -0300

    feat(player): add materials and controller adjusts.

--- src/Rundred Seconds/Assets/Scripts/PlayerController.cs
+++ src/Rundred Seconds/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Horizontal"))
+        {
+            float sense = Input.GetAxis("Horizontal");
+
+            if (sense > 0)
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            else if (sense < 0)
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            float sense = Input.GetAxis("Vertical");
+
+            if (sense > 0)
+            {
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            else if (sense < 0)
+            {
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
     }
 }
